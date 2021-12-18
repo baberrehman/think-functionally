c345f99f30b1153ba163a57b0ef6a6ab55131f8d
baberrehman <baberrehman26@gmail.com>
2021-12-19 02:58:46 +0800

    exercises ch2

--- ch2/exercises/c.hs
+++ ch2/exercises/c.hs
@@ -18,3 +18,31 @@ first p xs
   | p x = x
   | otherwise = first p (tail xs)
   where x = head xs
+
+-- Exercise E
+
+first' :: (a -> Bool) -> [a] -> Maybe a
+first' p xs
+  | null xs = Nothing
+  | p x = Just x
+  | otherwise = first' p (tail xs)
+  where x = head xs
+
+
+-- Exercise G
+
+type Date = (Int,Int,Int)
+
+months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+
+dayPostFix :: Int -> String
+dayPostFix x
+  | xmod == 1 && x /= 11 = show x ++ "st"
+  | xmod == 2 && x /= 12 = show x ++ "nd"
+  | xmod == 3 && x /= 13 = show x ++ "rd"
+  | otherwise = show x ++ "th"
+  where xmod = x `mod` 10
+  
+
+showDate :: Date -> String
+showDate (day, month, year) = dayPostFix day ++ " " ++ months !! (month-1) ++ ", " ++ show year
