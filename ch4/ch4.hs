5c097882209388c5a415f598f34b06b1d696f06c
baberrehman <baberrehman26@gmail.com>
2021-12-26 02:41:50 +0800

    ch4

--- ch4/ch4.hs
+++ ch4/ch4.hs
@@ -9,3 +9,11 @@ length' (x:xs) = 1 + length' xs
 length'' :: [a] -> Int
 length'' xs = sum $ [1 | x <- xs]
 
+zip' :: [a] -> [b] -> [(a,b)]
+zip' (x:xs) (y:ys) = (x,y): zip' xs ys
+zip' _ _ = []
+
+zipWith' :: (a -> b -> c) -> [a] -> [b] -> [c]
+zipWith' f (x:xs) (y:ys) = f x y : zipWith' f xs ys
+zipWith' _ _ _ = []
+
